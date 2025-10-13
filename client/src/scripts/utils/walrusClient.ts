a82a1ac004a28816f630eb699a6b9bca6f7bd61a
EduinoMEH <eemedranovillela@ucsb.edu>
2025-10-13 14:56:30 -0700

    removed unnecessary import for walrusClient

--- client/src/scripts/utils/walrusClient.ts
+++ client/src/scripts/utils/walrusClient.ts
@@ -3,7 +3,6 @@ import { WalrusClient } from "@mysten/walrus";
 import { Ed25519Keypair } from "@mysten/sui/keypairs/ed25519";
 import dotenv from "dotenv";
 import path from "path";
-import { fileURLToPath } from "url";
 import { setDefaultResultOrder } from "dns";
 
 setDefaultResultOrder('ipv4first');
