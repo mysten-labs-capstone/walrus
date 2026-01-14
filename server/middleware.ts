221566d3e37d5d9e858cb6ad928361f4467e2ae8
AwinZhang <infinitycomplix@gmail.com>
2026-01-13 20:46:14 -0800

    bugfix: history not showing correct time
    added epochs field to prisma schema
    fixed history showing incorrect time duration

--- server/middleware.ts
+++ server/middleware.ts
@@ -3,7 +3,11 @@ import type { NextRequest } from 'next/server';
 
 const STATIC_ALLOWED = new Set<string>([
   'http://localhost:5173',
+  'http://localhost:5174',
+  'http://localhost:5175',
   'http://127.0.0.1:5173',
+  'http://127.0.0.1:5174',
+  'http://127.0.0.1:5175',
   'https://mysten-labs-capstone.netlify.app',
 ]);
 
