fc2a0d08b891da8d6a06918f8e384be28a928186
kjlee2504 <kjlee@ucsb.edu>
2026-01-28 20:02:33 -0800

    updated logo

--- client/src/pages/Join.tsx
+++ client/src/pages/Join.tsx
@@ -212,10 +212,13 @@ export const Join: React.FC = () => {
         <div className="container">
           <div className="login-logo">
             <div className="logo-row">
-              <div className="logo-mark">
-                <span>W</span>
-              </div>
-              <h1 className="logo-title">Infinity Storage</h1>
+              <a href="/" className="logo-mark-link">
+                <img
+                  src="/logo+text.svg"
+                  alt="Walrus - Infinity Storage"
+                  className="login-logo-img h-12 w-auto"
+                />
+              </a>
             </div>
           </div>
 
