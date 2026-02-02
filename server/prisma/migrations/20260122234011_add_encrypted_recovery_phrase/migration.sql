07b31429c8367ad8bf8cbeb095043335c5c4acd0
EduinoMEH <eemedranovillela@ucsb.edu>
2026-02-01 16:22:31 -0800

    repaired issues present with prisma database, and added additional package for future assistsnace with postgreSQL databases

--- server/prisma/migrations/20260122234011_add_encrypted_recovery_phrase/migration.sql
+++
@@ -1,9 +0,0 @@
-/*
-  Warnings:
-
-  - You are about to drop the column `privateKey` on the `User` table. All the data in the column will be lost.
-
-*/
--- AlterTable
-ALTER TABLE "User" DROP COLUMN "privateKey",
-ADD COLUMN     "encryptedRecoveryPhrase" TEXT;
--- server/prisma/migrations/20260123194234_add_argon2_encryption_fields/migration.sql
+++
@@ -1,6 +0,0 @@
--- AlterTable
-ALTER TABLE "User" 
-ADD COLUMN "authKeyHash" TEXT,
-ADD COLUMN "salt" TEXT,
-ADD COLUMN "encryptedMasterKey" TEXT,
-ALTER COLUMN "passwordHash" DROP NOT NULL;
